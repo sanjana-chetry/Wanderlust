f94434849157e2aacf30fb490d21288bf0ffdd64
sanjana-chetry <sanjanachetry118@gmail.com>
2026-06-19 12:55:24 +0530

    Display image

--- controllers/listings.js
+++ controllers/listings.js
@@ -27,10 +27,14 @@ module.exports.showListing = (async (req,res)=>{
 })
 
 module.exports.createListing = async(req,res,next)=>{
-    let newListing = new Listing(req.body.listing);
+    let url = req.file.path;
+    let filename = req.file.filename;
+
+    const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    req.flash("success","New Listing Created!");
+    newListing.image = {url, filename};
     await newListing.save();
+    req.flash("success","New Listing Created!");
     res.redirect("/listings");
 }
 
